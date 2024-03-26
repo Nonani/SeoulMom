e9239dcb399759c1c90e527e44c38688e1a2f3c3
Jinwoo Choi <cjw1234577@gmail.com>
2024-03-26 13:53:34 +0900

    add: mainpage

--- untitled/lib/presentation/views/HomePage.dart
+++ untitled/lib/presentation/views/HomePage.dart
@@ -1,67 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_awesome_select/flutter_awesome_select.dart';
+import 'package:multi_select_flutter/bottom_sheet/multi_select_bottom_sheet.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:multi_select_flutter/util/multi_select_item.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   final _panelController = PanelController();
-  List<String> value = [];
-  List<S2Choice<String>> frameworks = [
-    S2Choice<String>(value: "1", title: 'Ionic'),
-    S2Choice<String>(value: "2", title: 'Flutter'),
-    S2Choice<String>(value: "3", title: 'React Native'),
+  List<String?> selectedItems = [];
+  List<MultiSelectItem<String>> _items = [
+    MultiSelectItem<String>('1', '육아'),
+    MultiSelectItem<String>('2', '출산'),
+    MultiSelectItem<String>('3', '출산'),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: ElevatedButton(
+        appBar: AppBar(
+            title: ElevatedButton(
           onPressed: () {
-            _panelController.open();
+            _showMultiSelect(context);
           },
           child: Text("Scroll to top"),
-        )
-      ),
-      body:Column(
-        children: [
-          SmartSelect<String>.multiple(
-            title: 'Days',
-            selectedValue: value,
-            onChange: (selected) => setState(() => value = selected.value),
-            choiceItems: frameworks,
-            choiceType: S2ChoiceType.chips,
-            choiceStyle: S2ChoiceStyle(
-              outlined: true,
-            ),
-            choiceActiveStyle: S2ChoiceStyle(
-              raised: true,
-            ),
-            modalType: S2ModalType.bottomSheet,
-            tileBuilder: (context, state) {
-              return S2Tile.fromState(
-                state,
-                isTwoLine: true,
-                leading: Container(
-                  width: 40,
-                  alignment: Alignment.center,
-                  child: const Icon(Icons.calendar_today),
-                ),
-              );
+        )),
+        body: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  print(selectedItems);
+                },
+                child: Text("Print value")),
+          ],
+        ));
+  }
+
+  void _showMultiSelect(BuildContext context) async {
+    await showModalBottomSheet(
+      isScrollControlled: false, // required for min/max child size
+      context: context,
+      builder: (ctx) {
+        return Wrap(children: [
+          MultiSelectChipField(
+            items: _items,
+            initialValue: selectedItems,
+
+            onTap: (values) {
+              selectedItems = values;
             },
+            chipWidth: 50,
+            textStyle: TextStyle(fontSize: 16),
           ),
-          ElevatedButton(onPressed: (){
-            print(value);
-          }, child: Text("Print value")),
-        ],
-      )
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Print value"))
+        ]);
+      },
     );
+    setState(() {
+    });
   }
 }
